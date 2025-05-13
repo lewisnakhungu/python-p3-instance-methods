552260939b8f320cef1a8b676e5f15deebb8e6e3
LewisRobertNakhungu <lokiyule@gmail.com>
2025-05-14 02:33:34 +0300

    complete instances

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,21 @@
 
 class Dog:
     # Class body goes here
+    
+    def bark(self):
+        print ("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+    
+
+    #Instance method 
+fido = Dog()
+fido.bark()
+fido.sit()
+
+    
+
+
+    
